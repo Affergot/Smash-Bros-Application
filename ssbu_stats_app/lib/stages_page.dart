fcd180da9756e74f7015f985128a1d015de84a7f
Affergot <ralphsartin721@gmail.com>
2023-03-05 00:21:48 -0600

    Worked on drop down list for character select

--- ssbu_stats_app/lib/stages_page.dart
+++ ssbu_stats_app/lib/stages_page.dart
@@ -1,7 +1,91 @@
 import 'package:flutter/material.dart';
 
 class StagePage extends StatelessWidget {
-  const StagePage({Key? key}) : super(key: key);
+  StagePage({Key? key}) : super(key: key);
+  String _selectedCharacter = '';
+  final List<String> _characterList = [
+    "Banjo & Kazooie",
+    "Bayonetta",
+    "Bowser",
+    "Bowser Jr.",
+    "Byleth",
+    "Captain Falcon",
+    "Chrom",
+    "Cloud",
+    "Corrin",
+    "Daisy",
+    "Dark Pit",
+    "Dark Samus",
+    "Diddy Kong",
+    "Donkey Kong",
+    "Dr. Mario",
+    "Duck Hunt",
+    "Falco",
+    "Fox",
+    "Ganondorf",
+    "Greninja",
+    "Hero",
+    "Ice Climbers",
+    "Ike",
+    "Incineroar",
+    "Inkling",
+    "Isabelle",
+    "Jigglypuff",
+    "Joker",
+    "Kazuya",
+    "Ken"
+        "King Dedede"
+        "King K. Rool"
+        "Kirby"
+        "Link"
+        "Little Mac"
+        "Lucario"
+        "Lucas"
+        "Lucina"
+        "Luigi"
+        "Mario"
+        "Marth"
+        "Mega Man"
+        "Meta Knight"
+        "Mewtwo"
+        "Mii Brawler"
+        "Mii Gunner"
+        "Mii Swordfighter"
+        "Min Min"
+        "Mr. Game & Watch"
+        "Ness"
+        "Olimar"
+        "Pac-Man"
+        "Palutena"
+        "Peach"
+        "Pichu"
+        "Pikachu"
+        "Piranha Plant"
+        "Pit"
+        "Pokémon Trainer"
+        "Richter"
+        "Ridley"
+        "Robin"
+        "Rosalina & Luma"
+        "Roy"
+        "Ryu"
+        "Samus"
+        "Sheik"
+        "Shulk"
+        "Simon"
+        "Snake"
+        "Sonic"
+        "Terry"
+        "Toon Link"
+        "Villager"
+        "Wario"
+        "Wii Fit Trainer"
+        "Wolf"
+        "Yoshi"
+        "Young Link"
+        "Zelda"
+        "Zero Suit Samus"
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -11,9 +95,25 @@ class StagePage extends StatelessWidget {
         backgroundColor: Colors.blue.shade900,
         foregroundColor: Colors.yellowAccent.shade400,
       ),
-      body: const Center(
-        child: Text('This is the stage picker page'),
+      body: Center(
+        child: DropdownButton<String>(
+          value: _selectedCharacter,
+          items: _characterList
+              .map<DropdownMenuItem<String>>(
+                  (String value) => DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      ))
+              .toList(),
+          onChanged: (String? newValue) {
+            setState(() {
+              _selectedCharacter = newValue!;
+            });
+          },
+        ),
       ),
     );
   }
+
+  void setState(Null Function() param0) {}
 }
